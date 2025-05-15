3a837e2f888d32023b3300b516ca2e34b6288596
psa <psapers@gmail.com>
2025-05-15 17:53:14 +0300

    Lesson 6 initial

--- src/components/logo/Logo.js
+++
@@ -1,9 +0,0 @@
-import styleLogo from './logo.module.css';
-
-export default function Logo() {
-  return  (
-    <div className={styleLogo.logo}>
-      LOGO
-    </div>
-  );
-}
--- src/components/tasks/description/Description.js
+++
@@ -1,9 +0,0 @@
-import descriptionStyle from './description.module.css';
-
-export default function description({description}) {
-  return (
-    <div className={descriptionStyle.container}>
-      {description}
-    </div>
-  );
-}
